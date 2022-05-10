eca9a149e0ba37770b486853afa97a486ebdc1e4
Maximilian Schüle <max.schuele@gmail.com>
2022-05-10 07:59:03 +0200

    -

---
+++ day01/part1/part1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+
+func main () {
+	path, _ := os.Getwd()
+	fmt.Println(path)
+}
--- util/util.go
+++ util/util.go
@@ -1,1 +1,13 @@
 package util
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func GetInput(day string) string {
+	path, _ := os.Getwd()
+	pathf, _ := os.ReadFile(filepath.Join(path, day, "input.txt"))
+	return strings.TrimSpace(string(pathf))
+}
