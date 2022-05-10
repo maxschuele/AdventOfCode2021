6b92f07e1a2d18923b9add18bab27b0db8b2db1c
Maximilian Schüle <max.schuele@gmail.com>
2022-05-10 13:56:09 +0200

    day3 pt2 ugly solution

--- day03/part2/part2.go
+++ day03/part2/part2.go
@@ -5,37 +5,85 @@ import (
 	"fmt"
 )
 
+type number struct {
+	value  int
+	marked bool
+}
+
 func main() {
 	ox, co2, mask := 0, 0, 2048
-	var numbers []int
+	numbers := []number{}
 	for _, v := range util.GetInputLines() {
-		numbers = append(numbers, util.ParseBinary(v))
+		n := number{value: util.ParseBinary(v), marked: false}
+		numbers = append(numbers, n)
 	}
+	numbers2 := make([]number, len(numbers))
+	copy(numbers2, numbers)
 
-	//check for common bit
 	for mask > 0 {
+		//oxygen generator
 		zero, one := 0, 0
 		for _, v := range numbers {
-			if v&mask == 0 {
+			if !v.marked && v.value&mask == 0 {
 				zero++
-			} else {
+			}
+			if !v.marked && v.value&mask != 0 {
 				one++
 			}
 		}
-		fmt.Printf("%d %d\n", one, zero)
-		for i, v := range numbers {
+		for i := 0; i < len(numbers); i++ {
+			n := &numbers[i]
+
 			if one >= zero {
-				if v&mask == 0 {
-					numbers = append(numbers[:i], numbers[i+1:]...)
+				if !n.marked && n.value&mask == 0 {
+					n.marked = true
 				}
 			} else {
-				if v&mask == 1 {
-					numbers = append(numbers[:i], numbers[i+1:]...)
+				if !n.marked && n.value&mask != 0 {
+					n.marked = true
 				}
 			}
 		}
+		for _, v := range numbers {
+			if !v.marked {
+				ox = v.value
+			}
+		}
+		//co2 scrubber
+		zero, one = 0, 0
+		for _, v := range numbers2 {
+			if !v.marked && v.value&mask == 0 {
+				zero++
+			}
+			if !v.marked && v.value&mask != 0 {
+				one++
+			}
+		}
+		for i := 0; i < len(numbers2); i++ {
+			n := &numbers2[i]
+			if one >= zero {
+				if !n.marked && n.value&mask != 0 {
+					n.marked = true
+				}
+			} else {
+				if !n.marked && n.value&mask == 0 {
+					n.marked = true
+				}
+			}
+		}
+		for _, v := range numbers2 {
+			if !v.marked {
+				co2 = v.value
+			}
+		}
+
+		//shift mask right one bit
 		mask = mask >> 1
 	}
-	fmt.Println(numbers)
+
+	
+
+	fmt.Println(ox)
+	fmt.Println(co2)
 	fmt.Printf("Result: %v\n", ox*co2)
 }
